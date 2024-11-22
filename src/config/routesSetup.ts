0b7c65425d3d2d7a8897a1b5d26e9976a07a9bd2
Adrian <marcelonads@gmail.com>
2024-11-22 10:15:28 +0800

    development: Added a setup file for all of the routes for api gateway

---
+++ src/config/routesSetup.ts
@@ -0,0 +1,58 @@
+import { Application, Request, Response } from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { rateLimit } from "express-rate-limit";
+
+// Routes
+import authenticationRoutes from "../routes/AuthenticationRoutes";
+import privateRoutes from "../routes/PrivateRoutes";
+
+// Middlewares
+import AuthenticationMiddleware from "../middlewares/AuthenticationMiddleware";
+import RoleMiddleware from "../middlewares/RoleMiddleware";
+
+interface ExtendedRequest extends Request {
+	username?: string;
+}
+
+export const setupAuthenticationRoutes = (app: Application) => {
+	authenticationRoutes.forEach((route) => {
+		app.use(route.path, createProxyMiddleware(route.proxy));
+	});
+};
+
+export const setupPrivateRoutes = (app: Application) => {
+	privateRoutes.forEach((route) => {
+		app.use(
+			route.path,
+			createProxyMiddleware({
+				...route.proxy,
+				on: {
+					proxyReq: (proxyReq, req: ExtendedRequest, res: Response) => {
+						proxyReq.setHeader("x-roles", JSON.stringify(route.roles));
+						proxyReq.setHeader("x-username", String(req.username));
+					},
+				},
+			})
+		);
+	});
+};
+
+export const setupAuthenticationMiddleware = (app: Application) => {
+	privateRoutes.forEach((route) => {
+		let middleware = new AuthenticationMiddleware();
+
+		if (route.auth) {
+			app.use(route.path, middleware.TokenMiddleware());
+		}
+	});
+};
+
+export const setupRoleManagementMiddleware = (app: Application) => {
+	privateRoutes.forEach((route) => {
+		let middleware = new RoleMiddleware();
+
+		if (route.auth) {
+			app.use(route.path, middleware.RoleCheckerMiddleware(route.roles));
+		}
+	});
+};
--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,25 @@
-import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 import express, { Application } from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
-import authenticationRoutes from "./routes/AuthenticationRoutes";
 import morgan from "morgan";
-import privateRoutes from "./routes/PrivateRoutes";
-import AuthenticationMiddleware from "./middlewares/AuthenticationMiddleware";
-import RoleMiddleware from "./middlewares/RoleMiddleware";
+
+import {
+	setupAuthenticationMiddleware,
+	setupAuthenticationRoutes,
+	setupPrivateRoutes,
+	setupRoleManagementMiddleware,
+} from "./config/routesSetup";
 
 const app: Application = express();
 
 app.use(morgan("combined"));
 
-privateRoutes.forEach((route) => {
-	let middleware = new AuthenticationMiddleware();
-
-	if (route.auth) {
-		app.use(route.path, middleware.TokenMiddleware());
-	}
-});
-
-privateRoutes.forEach((route) => {
-	let middleware = new RoleMiddleware();
-
-	if (route.auth) {
-		app.use(route.path, middleware.RoleCheckerMiddleware(route.roles));
-	}
-});
-
-authenticationRoutes.forEach((route) => {
-	app.use(route.path, createProxyMiddleware(route.proxy));
-});
-
-privateRoutes.forEach((route) => {
-	app.use(route.path, createProxyMiddleware(route.proxy));
-});
+setupAuthenticationMiddleware(app);
+setupRoleManagementMiddleware(app);
+setupAuthenticationRoutes(app);
+setupPrivateRoutes(app);
 
 app.listen(process.env.PORT, () => {
 	console.log("Server is running on port " + process.env.PORT);
